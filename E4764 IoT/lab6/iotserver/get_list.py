6d858136d2c00c3f243dc164625cef1e4ce3355b
EC2 Default User <ec2-user@ip-172-31-38-158.us-east-2.compute.internal>
2019-10-25 03:39:55 +0000

    walalalal

---
+++ E4764 IoT/lab6/iotserver/get_list.py
@@ -0,0 +1,52 @@
+import requests, json
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+import numpy
+def get_list():
+  url = 'http://18.218.34.7:8080/get'
+  r = requests.get(url)
+  co = json.loads(r.text)['result']
+
+  big = []
+  idx = 0
+  small =[] # 20 lists  
+  label = []
+  print(len(co))
+  while idx < len(co):
+    l = co[idx]['label']
+    label.append(l)
+    for i in range(20):
+      if idx > len(co)-1:
+         break
+      temp = []
+      xco = co[idx]['xcoordinate']
+      yco = co[idx]['ycoordinate']
+      temp.append(xco)
+      temp.append(yco)
+      small.append(temp)
+      idx += 1
+      #print(idx)
+
+  
+    big.append(small)
+    small = []
+    #print('--------')
+  
+  
+  #print("-------------------")
+  print(big)
+  print(len(big))
+  return big,label
+
+def train_model(train, label):
+    clf = RandomForestClassifier(n_estimators = 100)
+    clf.fit(train,label)
+    joblib.dump(clf,"random_forest_model.m")
+
+if __name__ == "__main__":
+    train , label = get_list()
+    train = numpy.array([x for x in train])
+    nsamples, nx, ny = train.shape
+    train = train.reshape((nsamples,nx*ny))
+    print(train)
+    train_model(train, label)
