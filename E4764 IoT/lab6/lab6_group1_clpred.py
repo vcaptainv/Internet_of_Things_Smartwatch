89d236afaff03a29b47d78dc02556ff2b0e6481c
Brian Wu <bwu@Brians-MacBook-Pro-2.local>
2019-10-27 16:12:17 -0400

    huzzah client training code + client prediction code

--- E4764 IoT/lab6/lab6_group1_clpred.py
+++ E4764 IoT/lab6/lab6_group1_clpred.py
@@ -1,12 +1,9 @@
 import network
-import socket
+from socket import socket, AF_INET, SOCK_STREAM
 import ssd1306
 import machine
 from machine import Pin
-import json
 import time
-import urequests
-import usocket
 import gc
 
 i2c = machine.I2C(-1, machine.Pin(5), machine.Pin(4))
@@ -14,10 +11,8 @@ oled = ssd1306.SSD1306_I2C(128, 32, i2c)
 spi = machine.SPI(1, baudrate=2000000, polarity=1, phase=1)
 cs = machine.Pin(15, machine.Pin.OUT)
 button_press = 0
-Amazon_server= "http://18.218.34.7/post"
-#json_list = []
-letters = ["C","O","L","U","M","B","I","A"]
-#letters = ["C","O","L","U"]
+Amazon_server= "18.218.34.7"
+str_builder = ""
 
 def do_connect():
     sta_if = network.WLAN(network.STA_IF)
@@ -54,8 +49,8 @@ def press_button(p):
         button_press = 0
         #display_text("Stop")
 
-def data_collection(json_list):
-    global button_press
+def data_collection():
+    global button_press, str_builder
     #Read a number of bytes specified by nbytes while continuously writing
     #the single byte given by write. Returns a bytes object with the data that was read.
     cs.value(0)
@@ -66,8 +61,7 @@ def data_collection(json_list):
     y = spi.read(5, 0xf5) # register 35: Y-Axis Data 1 # read 5 bytes
     cs.value(1)
     display_text(str(x[1])+","+str(y[1]))
-    json_data = {"x":x[1],"y":y[1]}
-    json_list.append(json_data)
+    str_builder += (str(x[1])+","+str(y[1])+",")
     time.sleep(0.15)
 
 def start_accel():
@@ -86,44 +80,32 @@ def memfree():
     gc.mem_free()
 
 def main():
-    global button_press, letters
+    global button_press, str_builder
     start_accel()
     button_B = Pin(2, Pin.IN, Pin.PULL_UP)
     button_B.irq(trigger=Pin.IRQ_FALLING, handler=press_button)
     ip_addr = do_connect()
-    #"columbia" has 8 letters
-    for letter in range(8):
-        json_list = []
-        display_text("Data for {} ...".format(letters[letter]))
+    display_text("training")
+    while True:
         while True:
             if button_press:
                 for i in range(20):
-                    data_collection(json_list)
+                    data_collection()
                 break
+
+        serverName = Amazon_server
+        serverPort = 8080
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((serverName,serverPort))
+        clientSocket.send(str_builder.encode('utf-8'))
+
+        response = clientSocket.recv(1024)
+        display_text(str(response.decode('utf-8')))
         
-        display_text("Collected {} ...".format(letters[letter]))
-        time.sleep(1)
-        display_text("start sending ...")
-        time.sleep(1)
-
-        for i in range (len(json_list)):
-            x = json_list[i]['x']
-            y = json_list[i]['y']
-            display_text(str(x)+","+str(y))
-
-            _, _, host, path = Amazon_server.split('/', 3)
-            addr = usocket.getaddrinfo(host, 8080)[0][-1]
-            s = usocket.socket()
-            s.connect(addr)
-            post_json = '{"label": '+ str(letter) + ', "x": ' + str(x) + ', "y": ' + str(y) + '}'
-
-            post = 'POST /%s HTTP/1.1\r\nContent-length: %d\r\nContent-Type: application/json\r\nHost: %s\r\n\r\n%s' %(path, len(post_json), host, post_json)
-
-            s.send(post.encode('utf-8'))
-            memfree()
-            time.sleep(0.1)
         button_press = 0
-        display_text("Finish sending {}".format(letters[letter]))
-
+        display_text("Predict {}".format(response))
+        str_builder = ""
+        clientSocket.close()
+        
 if __name__ == '__main__':
     main()
